11d36f67d463e28ca8375d6b64337ff225caa207
Rafael <rafaeldacunhatheodoro@gmail.com>
2021-03-13 07:50:32 -0300

    Inicializando o MongoDB

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,24 @@
 
 import {Router} from 'express';
 
+import  mongoose  from  'mongoose';
+
+
+
+//Conexão com banco de dados
+mongoose.connect('mongodb://localhost/rafael', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() =>{
+    console.log("Conexão com sucesso.")
+}).catch((erro)=>{
+    console.log("erro nao foi conectado" + erro);
+});
+
 const routes = new Router();
 
 routes.get('/contatos', (req, res,) =>{
-    res.send("Naruto, Hashirama, Minato");
+    res.send("Rafael");
 })
 
 //module.exports = routes;
