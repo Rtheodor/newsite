51cf5b77c22ce1cd1c5e62478d04964c77e4b923
Rafael <rafaeldacunhatheodoro@gmail.com>
2021-03-18 18:24:44 -0300

    Validador de campos funcionando!

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-/*import {Router} from 'express';
+import {Router} from 'express';
 
 import  mongoose  from  'mongoose';
 
@@ -11,7 +11,7 @@ const routes = new Router();
 routes.post('/users', UserController.store);
 
 
-routes.get('/', async(req, res,) =>{
+/*routes.get('/', async(req, res,) =>{
     await User.create({
         nome: 'Rafael',
         email: 'rafaeldacunhatheodoro@gmail.com',
@@ -25,29 +25,4 @@ routes.get('/', async(req, res,) =>{
 
 //module.exports = routes;
 
-/*export default routes;*/
-
-import { Router } from 'express';
-import mongoose from 'mongoose';
-
-import UserController from './app/controllers/UserController';
-
-const routes = new Router();
-
-routes.post('/users', UserController.store);
-
-/*routes.get('/', async (req, res) => {
-    await User.create({
-        nome: 'Cesar3',
-        email: 'cesar@celke.com.br',
-        senha: '123456'
-    }, function(err, small){
-        if(err) return res.status(400).json({error: "Erro: Usuário não foi cadstrado com sucesso!"});
-
-        return res.status(200).json({error: "Usuário cadastrado com sucesso!"});
-    });
-    
-})*/
-
-//module.exports = routes;
 export default routes;
